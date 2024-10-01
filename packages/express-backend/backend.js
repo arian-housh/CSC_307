a7f8c9f5f486b39b934575e04b7a3e3177b9668a
arian_housh <arianhoush3@gmail.com>
2024-10-01 10:14:32 -0700

    step 5 done: getting users by id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -36,17 +36,22 @@ const users = {
     ]
   };
 
-app.get("/", (req, res) => {
-    res.send("Hello World!!!");
-  });
-
-  const findUserByName = (name) => {
+const findUserByName = (name) => {
     return users["users_list"].filter(
       (user) => user["name"] === name
     );
   };
+
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+
+
+app.get("/", (req, res) => {
+        res.send("Hello World!!!");
+    });
+
   
-  app.get("/users", (req, res) => {
+app.get("/users", (req, res) => {
     const name = req.query.name;
     if (name != undefined) {
       let result = findUserByName(name);
@@ -54,6 +59,16 @@ app.get("/", (req, res) => {
       res.send(result);
     } else {
       res.send(users);
+    }
+  });
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
     }
   });
 
